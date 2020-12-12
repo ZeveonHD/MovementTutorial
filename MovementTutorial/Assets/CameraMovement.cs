fe483e45bf6fece6edcafd575bc89f58f2a06671
ZeveonHD <roverdeer@outlook.com>
2020-12-12 11:18:20 -0800

    Tutorial Finished

---
+++ MovementTutorial/Assets/CameraMovement.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    public Transform playerBody;
+    public Transform cameraHolder;
+
+    public float sens;
+
+    public float currentY;
+
+    public void Start()
+    {
+        currentY = 0;
+    }
+
+    public void Update()
+    {
+        float inputX = Input.GetAxisRaw("Mouse X");
+        float inputY = Input.GetAxisRaw("Mouse Y");
+
+        currentY -= inputY;
+        currentY = Mathf.Clamp(currentY, -90, 90);
+
+        if (currentY < -90)
+        {
+            currentY = -89.9f;
+        }
+        else if (currentY > 90)
+        {
+            currentY = 89.9f;
+        }
+
+        playerBody.Rotate(new Vector3(0, inputX * Time.deltaTime * sens));
+        cameraHolder.localRotation = Quaternion.Euler(new Vector3(currentY, 0, 0));
+    }
+}
---
+++ MovementTutorial/Assets/MovementScript.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementScript : MonoBehaviour
+{
+    public float movementSpeed = 1f;
+    public float jumpPower = 10f;
+
+    public float gravityFactor = -9.81f;
+    public float currentVelY = 0;
+
+    public CharacterController controller;
+
+    public LayerMask groundMask;
+    public Transform groundDetectionTransform;
+
+    public bool isGrounded;
+
+    public void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    public void CheckIsGrounded()
+    {
+        Collider[] cols = Physics.OverlapSphere(groundDetectionTransform.position, 0.05f, groundMask);
+
+        if (cols.Length > 0)
+        {
+            isGrounded = true;
+        }
+        else
+        {
+            isGrounded = false;
+        }
+    }
+
+    public void Update()
+    {
+        float inputX = Input.GetAxisRaw("Horizontal");
+        float inputY = Input.GetAxisRaw("Vertical");
+        CheckIsGrounded();
+
+        if (isGrounded == false)
+        {
+            currentVelY += gravityFactor * Time.deltaTime;
+        }
+        else if (isGrounded == true)
+        {
+            currentVelY = -2f;
+        }
+
+        if (Input.GetKeyDown("space") && isGrounded == true)
+        {
+            currentVelY = jumpPower;
+        }
+
+        Vector3 movement = new Vector3();
+
+        movement = inputX * transform.right + inputY * transform.forward;
+
+        controller.Move(movement * movementSpeed * Time.deltaTime);
+        controller.Move(new Vector3(0,currentVelY * Time.deltaTime,0));
+    }
+}
